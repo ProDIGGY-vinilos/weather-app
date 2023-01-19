27dab9d814ac05cb44d5106c734bdb208db44ce5
ProDIGGY-vinilos <cristiangerster@gmail.com>
2023-01-19 12:50:13 -0300

    added my logo and some responsive aspects

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import styles from './App.module.css';
 import Header from './components/header/header';
 import CityForm from 'components/city-form/city-form';
 
-function App () {
+function App() {
 	return (
 		<section className={styles.container}>
 			<Header />
